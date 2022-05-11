6db35b0a076cea2a62cc66b0a243091fb359e2b7
Tripat-Jot <triptjot3@gmail.com>
2022-05-11 08:31:41 +0530

    adding files

---
+++ dbconfig.js
@@ -0,0 +1,16 @@
+	
+const config = {
+    user: "user",
+    password: "user",
+    server: "DESKTOP-CFHSA58",
+    database: "db_demo",
+    options: {
+      trustedconnection: true,
+      enableArithAbort: true,
+      instancename: "",
+    },
+    port: 49685
+  };
+   
+  module.exports = config;
+  
