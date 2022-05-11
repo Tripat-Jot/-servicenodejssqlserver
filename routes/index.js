6f5e32f2616887c27bf7154d830c385bcb51d0dc
Tripat-Jot <triptjot3@gmail.com>
2022-05-11 18:50:37 +0530

    Updating

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,18 @@ const sql = require("../dboperation");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send('<p><h2>Welcome to Azure by Tripatjot Singh </h2></p> <p><a href="/getdata_withQuery"> Data Extracted from Stored Procedure </a>  <p><a href="/testconnect"> Check Server Connection </a> </p>') ;
 });
 
 
 //test connection
 router.get('/testconnect', function(req, res, next) {
+  
   sql.getdata();
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send ("Server is connected !!!")
+  // res.send(result[0]) ;
 });
 
 
